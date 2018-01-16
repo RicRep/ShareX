3d3cb9a5f448b2fe7fc72fc613a09a45c477c943
Richard Repp <rrepp@codewrights.de>
2018-01-16 12:31:27 +0100

    Splitted ImageHelper Class

---
+++ ShareX.HelpersLib/Helpers/ImageDrawHelpers.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShareX.HelpersLib.Helpers
+{
+    public static class ImageDrawHelpers
+    {
+        private const InterpolationMode DefaultInterpolationMode = InterpolationMode.HighQualityBicubic;
+
+        public static Image DrawBorder(Image img, Color borderColor, int borderSize, BorderType borderType)
+        {
+            using (Pen borderPen = new Pen(borderColor, borderSize) { Alignment = PenAlignment.Inset })
+            {
+                return DrawBorder(img, borderPen, borderType);
+            }
+        }
+
+        public static Image DrawBorder(Image img, Color fromBorderColor, Color toBorderColor, LinearGradientMode gradientType, int borderSize, BorderType borderType)
+        {
+            int width = img.Width;
+            int height = img.Height;
+
+            if (borderType == BorderType.Outside)
+            {
+                width += borderSize * 2;
+                height += borderSize * 2;
+            }
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(new Rectangle(0, 0, width, height), fromBorderColor, toBorderColor, gradientType))
+            using (Pen borderPen = new Pen(brush, borderSize) { Alignment = PenAlignment.Inset })
+            {
+                return DrawBorder(img, borderPen, borderType);
+            }
+        }
+
+        public static Image DrawBorder(Image img, Pen borderPen, BorderType borderType)
+        {
+            Bitmap bmp;
+
+            if (borderType == BorderType.Inside)
+            {
+                bmp = (Bitmap)img;
+
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.DrawRectangleProper(borderPen, 0, 0, img.Width, img.Height);
+                }
+            }
+            else
+            {
+                int borderSize = (int)borderPen.Width;
+                bmp = img.CreateEmptyBitmap(borderSize * 2, borderSize * 2);
+
+                using (Graphics g = Graphics.FromImage(bmp))
+                using (img)
+                {
+                    g.DrawRectangleProper(borderPen, 0, 0, bmp.Width, bmp.Height);
+                    g.SetHighQuality();
+                    g.DrawImage(img, borderSize, borderSize, img.Width, img.Height);
+                }
+            }
+
+            return bmp;
+        }
+
+        public static Image DrawCheckers(Image img)
+        {
+            return DrawCheckers(img, 10, Color.FromArgb(230, 230, 230), Color.White);
+        }
+
+        public static Image DrawCheckers(Image img, int size, Color color1, Color color2)
+        {
+            Bitmap bmp = img.CreateEmptyBitmap();
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (Image checker = ImageHelpers.CreateCheckerPattern(size, size, color1, color2))
+            using (Brush checkerBrush = new TextureBrush(checker, WrapMode.Tile))
+            using (img)
+            {
+                g.FillRectangle(checkerBrush, new Rectangle(0, 0, bmp.Width, bmp.Height));
+                g.SetHighQuality();
+                g.DrawImage(img, 0, 0, img.Width, img.Height);
+            }
+
+            return bmp;
+        }
+
+        public static Image DrawCheckers(int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (Image checker = ImageHelpers.CreateCheckerPattern())
+            using (Brush checkerBrush = new TextureBrush(checker, WrapMode.Tile))
+            {
+                g.FillRectangle(checkerBrush, new Rectangle(0, 0, bmp.Width, bmp.Height));
+            }
+
+            return bmp;
+        }
+
+        public static void DrawColorPickerIcon(Graphics g, Color color, Rectangle rect, int holeSize = 0)
+        {
+            if (color.A < 255)
+            {
+                using (Image checker = ImageHelpers.CreateCheckerPattern(rect.Width / 2, rect.Height / 2))
+                {
+                    g.DrawImage(checker, rect);
+                }
+            }
+
+            using (SolidBrush brush = new SolidBrush(color))
+            {
+                g.FillRectangle(brush, rect);
+            }
+
+            g.DrawRectangleProper(Pens.Black, rect);
+
+            if (holeSize > 0)
+            {
+                g.CompositingMode = CompositingMode.SourceCopy;
+
+                Rectangle holeRect = new Rectangle(rect.Width / 2 - holeSize / 2, rect.Height / 2 - holeSize / 2, holeSize, holeSize);
+
+                g.FillRectangle(Brushes.Transparent, holeRect);
+                g.DrawRectangleProper(Pens.Black, holeRect);
+            }
+        }
+
+        public static Image DrawReflection(Image img, int percentage, int maxAlpha, int minAlpha, int offset, bool skew, int skewSize)
+        {
+            Bitmap reflection = ImageHelpers.AddReflection(img, percentage, maxAlpha, minAlpha);
+
+            if (skew)
+            {
+                reflection = ImageHelpers.AddSkew(reflection, skewSize, 0);
+            }
+
+            Bitmap result = new Bitmap(reflection.Width, img.Height + reflection.Height + offset);
+
+            using (Graphics g = Graphics.FromImage(result))
+            using (img)
+            using (reflection)
+            {
+                g.SetHighQuality();
+                g.DrawImage(img, 0, 0, img.Width, img.Height);
+                g.DrawImage(reflection, 0, img.Height + offset, reflection.Width, reflection.Height);
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ ShareX.HelpersLib/Helpers/ImageResizeHelpers.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShareX.HelpersLib.Helpers
+{
+    public static class ImageResizeHelpers
+    {
+        private const InterpolationMode DefaultInterpolationMode = InterpolationMode.HighQualityBicubic;
+
+        public static Image ResizeImage(Image img, int width, int height, InterpolationMode interpolationMode = DefaultInterpolationMode)
+        {
+            if (width < 1 || height < 1 || (img.Width == width && img.Height == height))
+            {
+                return img;
+            }
+
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            bmp.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+            using (img)
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.InterpolationMode = interpolationMode;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.CompositingMode = CompositingMode.SourceOver;
+
+                using (ImageAttributes ia = new ImageAttributes())
+                {
+                    ia.SetWrapMode(WrapMode.TileFlipXY);
+                    g.DrawImage(img, new Rectangle(0, 0, width, height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+                }
+            }
+
+            return bmp;
+        }
+
+        public static Image ResizeImage(Image img, Size size, InterpolationMode interpolationMode = DefaultInterpolationMode)
+        {
+            return ResizeImage(img, size.Width, size.Height, interpolationMode);
+        }
+
+        public static Image ResizeImageByPercentage(Image img, float percentageWidth, float percentageHeight, InterpolationMode interpolationMode = DefaultInterpolationMode)
+        {
+            int width = (int)Math.Round(percentageWidth / 100 * img.Width);
+            int height = (int)Math.Round(percentageHeight / 100 * img.Height);
+            return ResizeImage(img, width, height, interpolationMode);
+        }
+
+        public static Image ResizeImageByPercentage(Image img, float percentage, InterpolationMode interpolationMode = DefaultInterpolationMode)
+        {
+            return ResizeImageByPercentage(img, percentage, percentage, interpolationMode);
+        }
+
+        public static Image ResizeImage(Image img, Size size, bool allowEnlarge, bool centerImage = true)
+        {
+            return ResizeImage(img, size.Width, size.Height, allowEnlarge, centerImage);
+        }
+
+        public static Image ResizeImage(Image img, int width, int height, bool allowEnlarge, bool centerImage = true)
+        {
+            return ResizeImage(img, width, height, allowEnlarge, centerImage, Color.Transparent);
+        }
+
+        public static Image ResizeImage(Image img, int width, int height, bool allowEnlarge, bool centerImage, Color backColor)
+        {
+            double ratio;
+            int newWidth, newHeight;
+
+            if (!allowEnlarge && img.Width <= width && img.Height <= height)
+            {
+                ratio = 1.0;
+                newWidth = img.Width;
+                newHeight = img.Height;
+            }
+            else
+            {
+                double ratioX = (double)width / img.Width;
+                double ratioY = (double)height / img.Height;
+                ratio = ratioX < ratioY ? ratioX : ratioY;
+                newWidth = (int)(img.Width * ratio);
+                newHeight = (int)(img.Height * ratio);
+            }
+
+            int newX = 0;
+            int newY = 0;
+
+            if (centerImage)
+            {
+                newX += (int)((width - (img.Width * ratio)) / 2);
+                newY += (int)((height - (img.Height * ratio)) / 2);
+            }
+
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            bmp.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (img)
+            {
+                g.Clear(backColor);
+                g.SetHighQuality();
+                g.DrawImage(img, newX, newY, newWidth, newHeight);
+            }
+
+            return bmp;
+        }
+
+        public static Image ResizeImageLimit(Image img, Size size)
+        {
+            return ResizeImageLimit(img, size.Width, size.Height);
+        }
+
+        /// <summary>If image size bigger than "size" then resize it and keep aspect ratio else return image.</summary>
+        public static Image ResizeImageLimit(Image img, int width, int height)
+        {
+            if (img.Width <= width && img.Height <= height)
+            {
+                return img;
+            }
+
+            int newWidth, newHeight;
+            double ratioX = (double)width / img.Width;
+            double ratioY = (double)height / img.Height;
+
+            if (ratioX < ratioY)
+            {
+                newWidth = width;
+                newHeight = (int)(img.Height * ratioX);
+            }
+            else
+            {
+                newWidth = (int)(img.Width * ratioY);
+                newHeight = height;
+            }
+
+            return ResizeImage(img, newWidth, newHeight);
+        }
+
+        public static Image ResizeImageLimit(Image img, int maxSize)
+        {
+            double ratio = (double)img.Width / img.Height;
+            double x = Math.Sqrt(maxSize / ratio);
+
+            int width, height;
+            if (ratio > 1)
+            {
+                width = (int)(ratio * x);
+                height = (int)(width / ratio);
+            }
+            else
+            {
+                height = (int)(ratio * x);
+                width = (int)(height / ratio);
+            }
+
+            return ResizeImage(img, width, height);
+        }
+    }
+}
